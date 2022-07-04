65d431d7796b2c844cb3167e482318c943909b20
SomedayAvA <102339920+SomedayAvA@users.noreply.github.com>
2022-07-04 23:50:59 +0800

    Update DNSRelay.java

--- DNSRelay.java
+++ DNSRelay.java
@@ -1,6 +1,11 @@
-public class DNSRelay {
+package DNS;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class dnsrelay {
     public static void main(String[] args){
-        UDPServer udp=new UDPServer();
-        udp.start();
+        ExecutorService sPool = Executors.newFixedThreadPool(20);
+        UDPServer udp= new UDPServer();
+        sPool.execute(udp);
     }
 }
