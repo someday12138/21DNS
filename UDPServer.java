93fc48d915053e02c0944a6cde56aed296604efa
SomedayAvA <102339920+SomedayAvA@users.noreply.github.com>
2022-07-05 17:45:39 +0800

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -1,4 +1,3 @@
-package DNS;
 import org.xbill.DNS.*;
 import org.xbill.DNS.Record;
 import java.io.*;
@@ -6,93 +5,91 @@ import java.net.*;
 import java.util.HashMap;
 
 
+
 public class UDPServer implements Runnable{
-    private static DatagramSocket socket;
+    private final DatagramPacket request;
+    private final DatagramSocket socket;
+
+
     private HashMap<String ,String> ipmap=map();
-    public UDPServer() {
-        //设置socket，监听端口53
-        try {
-            socket = new DatagramSocket(53);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+    public UDPServer(DatagramPacket p,DatagramSocket s) {
+        request = p;
+        socket = s;
     }
 
     public void run() {
-        System.out.println("Starting......\n");
-        int count=0;
-        while (true) {
-            System.out.println("=======================");
-            try {
-                byte[] buffer = new byte[1024];
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                //卡住直到收到receive
-                socket.receive(request);
-                //输出客户端的dns请求数据
-                InetAddress sourceIpAddr = request.getAddress();
-                int sourcePort = request.getPort();
-                System.out.println("\nsourceIpAddr = " + sourceIpAddr.toString() + "\nsourcePort = " + sourcePort);
-                //分析dns数据包格式
-                Message indata = new Message(request.getData());
-                System.out.println("\nindata = " + indata);
-                String ym=indata.getQuestion().getName().toString();
-                ym=ym.substring(0,ym.length()-1);
-                System.out.println("ym:::"+ym);
-                Record question = indata.getQuestion();
-                System.out.println("question = " + question);
-                String domain = indata.getQuestion().getName().toString();
-                System.out.println("domain = " + domain);
-                //解析域名
-                if(ipmap.containsKey(ym)){
+         {
 
-                    InetAddress answerIpAddr = InetAddress.getByName(ipmap.get(ym));
+            System.out.println(Thread.currentThread().getName());
+            int count = 0;
+             {
+                System.out.println("=======================");
 
-                    Message outdata = (Message)indata.clone();
-                    if(ipmap.get(ym).contains("0.0.0.0")) {
+                try {
+
+                    //输出客户端的dns请求数据
+                    InetAddress sourceIpAddr = request.getAddress();
+                    int sourcePort = request.getPort();
+                    System.out.println("\nsourceIpAddr = " + sourceIpAddr.toString() + "\nsourcePort = " + sourcePort);
+                    //分析dns数据包格式
+                    Message indata = new Message(request.getData());
+                    System.out.println("\nindata = " + indata);
+                    String ym = indata.getQuestion().getName().toString();
+                    ym = ym.substring(0, ym.length() - 1);
+                    System.out.println("ym:::" + ym);
+                    Record question = indata.getQuestion();
+                    System.out.println("question = " + question);
+                    String domain = indata.getQuestion().getName().toString();
+                    System.out.println("domain = " + domain);
+                    //解析域名
+                    if (ipmap.containsKey(ym)) {
+
+                        InetAddress answerIpAddr = InetAddress.getByName(ipmap.get(ym));
+
+                        Message outdata = (Message) indata.clone();
+                        if (ipmap.get(ym).contains("0.0.0.0")) {
+                            System.out.println("**********************************************************");
+                            Header reply = outdata.getHeader();
+                            reply.setRcode(3);
+                            outdata.setHeader(reply);
+                        }
                         System.out.println("**********************************************************");
-                        Header reply = outdata.getHeader();
-                        reply.setRcode(3);
-                        outdata.setHeader(reply);
+                        //由于接收到的请求为A类型，因此应答也为ARecord。查看Record类的继承，发现还有AAAARecord(ipv6)，CNAMERecord等
+                        Record answer = new ARecord(question.getName(), question.getDClass(), 64, answerIpAddr);
+                        outdata.addRecord(answer, Section.ANSWER);
+                        //发送消息给客户端
+                        System.out.println("data:" + outdata);
+                        byte[] buf = outdata.toWire();
+                        DatagramPacket response = new DatagramPacket(buf, buf.length, sourceIpAddr, sourcePort);
+                        socket.send(response);
+                        System.out.println("socket:" + socket);
+                    } else {
+                        InetAddress answerIpAddr = InetAddress.getByName(domain);
+                        Message outdata = (Message) indata.clone();
+                        //由于接收到的请求为A类型，因此应答也为ARecord。查看Record类的继承，发现还有AAAARecord(ipv6)，CNAMERecord等
+                        Record answer = new ARecord(question.getName(), question.getDClass(), 64, answerIpAddr);
+                        outdata.addRecord(answer, Section.ANSWER);
+                        //发送消息给客户端
+                        System.out.println("data:" + outdata);
+                        byte[] buf = outdata.toWire();
+                        DatagramPacket response = new DatagramPacket(buf, buf.length, sourceIpAddr, sourcePort);
+                        socket.send(response);
+                        System.out.println("socket:" + socket);
+                        if (count % 2 == 1) {
+                            ipmap = update(ipmap, ym, answerIpAddr.getHostAddress());
+                        }
                     }
-                    System.out.println("**********************************************************");
-                    //由于接收到的请求为A类型，因此应答也为ARecord。查看Record类的继承，发现还有AAAARecord(ipv6)，CNAMERecord等
-                    Record answer = new ARecord(question.getName(), question.getDClass(), 64, answerIpAddr);
-                    outdata.addRecord(answer, Section.ANSWER);
-                    //发送消息给客户端
-                    System.out.println("data:" + outdata);
-                    byte[] buf = outdata.toWire();
-                    DatagramPacket response = new DatagramPacket(buf, buf.length, sourceIpAddr, sourcePort);
-                    socket.send(response);
-                    System.out.println("socket:" + socket);
-                }
-                else
-                {
-                    InetAddress answerIpAddr = InetAddress.getByName(domain);
-                    Message outdata = (Message)indata.clone();
-                    //由于接收到的请求为A类型，因此应答也为ARecord。查看Record类的继承，发现还有AAAARecord(ipv6)，CNAMERecord等
-                    Record answer = new ARecord(question.getName(), question.getDClass(), 64, answerIpAddr);
-                    outdata.addRecord(answer, Section.ANSWER);
-                    //发送消息给客户端
-                    System.out.println("data:"+outdata);
-                    byte[] buf = outdata.toWire();
-                    DatagramPacket response = new DatagramPacket(buf, buf.length, sourceIpAddr, sourcePort);
-                    socket.send(response);
-                    System.out.println("socket:"+socket);
-                    if(count %2 ==1)
-                    {
-                        ipmap=update(ipmap,ym,answerIpAddr.getHostAddress());
-                    }
-                }
-                System.out.println(ipmap);
+                    System.out.println(ipmap);
 
-            } catch (SocketException e) {
-                System.out.println("SocketException:");
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("IOException:");
-                e.printStackTrace();
+                } catch (SocketException e) {
+                    System.out.println("SocketException:");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println("IOException:");
+                    e.printStackTrace();
+                }
+                count++;
             }
-            count++;
         }
     }
 
