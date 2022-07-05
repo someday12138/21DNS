3dfc81cca89bb9fa12923ce86d172a1f3ecd654b
BUPTwyz <108712448+BUPTwyz@users.noreply.github.com>
2022-07-05 13:09:53 +0800

    Update DNSRelay.java

--- DNSRelay.java
+++ DNSRelay.java
@@ -3,9 +3,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class dnsrelay {
+    //DNS
     public static void main(String[] args){
         ExecutorService sPool = Executors.newFixedThreadPool(20);
         UDPServer udp= new UDPServer();
         sPool.execute(udp);
-    }
+}
 }
